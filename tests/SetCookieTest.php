153c1f4a4ee6a3f4d7e51ea8c9a6e6a78566a6e3
Ricardo Fiorani <ricardo.fiorani@gmail.com>
2019-10-09 00:22:11 +0200

    improved tests coverage and added php versions assurance on travis

--- tests/SetCookieTest.php
+++ tests/SetCookieTest.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 
 namespace HansOtt\PSR7Cookies;
 
@@ -27,13 +28,17 @@ final class SetCookieTest extends \PHPUnit_Framework_TestCase
         $secure = true;
         $httpOnly = true;
         $sameSite = 'lax';
-        $setCookie = new SetCookie('name', 'value', $expiresAt, $path, $domain, $secure, $httpOnly, $sameSite);
-        $this->assertSame($expiresAt, $setCookie->expiresAt());
-        $this->assertSame($path, $setCookie->getPath());
-        $this->assertSame($domain, $setCookie->getDomain());
-        $this->assertSame($secure, $setCookie->isSecure());
-        $this->assertSame($httpOnly, $setCookie->isHttpOnly());
-        $this->assertSame($sameSite, $setCookie->getSameSite());
+        $name = 'me_likey';
+        $value = 'cookies';
+        $setCookie = new SetCookie($name, $value, $expiresAt, $path, $domain, $secure, $httpOnly, $sameSite);
+        self::assertSame($name, $setCookie->getName());
+        self::assertSame($name, $setCookie->getName());
+        self::assertSame($expiresAt, $setCookie->expiresAt());
+        self::assertSame($path, $setCookie->getPath());
+        self::assertSame($domain, $setCookie->getDomain());
+        self::assertSame($secure, $setCookie->isSecure());
+        self::assertSame($httpOnly, $setCookie->isHttpOnly());
+        self::assertSame($sameSite, $setCookie->getSameSite());
     }
 
     public function test_it_can_be_added_to_a_psr_response()
@@ -41,53 +46,117 @@ final class SetCookieTest extends \PHPUnit_Framework_TestCase
         $cookie = new SetCookie('name', 'value');
         $responseWithCookie = $cookie->addToResponse(new Response());
         $httpResponse = str($responseWithCookie);
-        $expected = 'HTTP/1.1 200 OK'."\r\n";
-        $expected .= 'Set-Cookie: name=value'."\r\n\r\n";
-        $this->assertEquals($expected, $httpResponse);
+        $expected = 'HTTP/1.1 200 OK' . "\r\n";
+        $expected .= 'Set-Cookie: name=value' . "\r\n\r\n";
+        self::assertEquals($expected, $httpResponse);
     }
 
     public function test_it_converts_to_header_value()
     {
         $cookie = new SetCookie('name', 'value');
-        $this->assertEquals('name=value', $cookie->toHeaderValue());
+        self::assertEquals('name=value', $cookie->toHeaderValue());
 
         $cookie = new SetCookie('NaMe', 'value', 0);
-        $this->assertEquals('NaMe=value', $cookie->toHeaderValue());
+        self::assertEquals('NaMe=value', $cookie->toHeaderValue());
 
         $cookie = new SetCookie('name', 'value with space');
-        $this->assertEquals('name=value+with+space', $cookie->toHeaderValue());
+        self::assertEquals('name=value+with+space', $cookie->toHeaderValue());
 
         $cookie = new SetCookie('name', 'value', 0, '/path/');
-        $this->assertEquals('name=value; path=/path/', $cookie->toHeaderValue());
+        self::assertEquals('name=value; path=/path/', $cookie->toHeaderValue());
 
         $cookie = new SetCookie('name', 'value', 0, '/path/', 'domain.tld');
-        $this->assertEquals('name=value; path=/path/; domain=domain.tld', $cookie->toHeaderValue());
+        self::assertEquals('name=value; path=/path/; domain=domain.tld', $cookie->toHeaderValue());
 
-        $cookie = new SetCookie('name', 'value', 0, '/path/', 'domain.tld', true);
-        $this->assertEquals('name=value; path=/path/; domain=domain.tld; secure', $cookie->toHeaderValue());
+        $cookie = new SetCookie(
+            'name',
+            'value',
+            0,
+            '/path/',
+            'domain.tld',
+            true
+        );
+        self::assertEquals('name=value; path=/path/; domain=domain.tld; secure', $cookie->toHeaderValue());
 
-        $cookie = new SetCookie('name', 'value', 0, '/path/', 'domain.tld', true, true);
-        $this->assertEquals('name=value; path=/path/; domain=domain.tld; secure; httponly', $cookie->toHeaderValue());
+        $cookie = new SetCookie(
+            'name',
+            'value',
+            0,
+            '/path/',
+            'domain.tld',
+            true,
+            true
+        );
+        self::assertEquals(
+            'name=value; path=/path/; domain=domain.tld; secure; httponly',
+            $cookie->toHeaderValue()
+        );
 
-        $cookie = new SetCookie('name', 'value', 1466459967, '', '', true, true);
-        $this->assertEquals('name=value; expires=Mon, 20 Jun 2016 21:59:27 GMT; secure; httponly', $cookie->toHeaderValue());
+        $cookie = new SetCookie(
+            'name',
+            'value',
+            1466459967,
+            '',
+            '',
+            true,
+            true
+        );
+        self::assertEquals(
+            'name=value; expires=Mon, 20 Jun 2016 21:59:27 GMT; secure; httponly',
+            $cookie->toHeaderValue()
+        );
 
-        $cookie = new SetCookie('name', 'value', 0, '/path/', 'domain.tld', true, true, 'strict');
-        $this->assertEquals('name=value; path=/path/; domain=domain.tld; secure; httponly; samesite=strict', $cookie->toHeaderValue());
+        $cookie = new SetCookie(
+            'name',
+            'value',
+            0,
+            '/path/',
+            'domain.tld',
+            true,
+            true,
+            'strict'
+        );
+        self::assertEquals(
+            'name=value; path=/path/; domain=domain.tld; secure; httponly; samesite=strict',
+            $cookie->toHeaderValue()
+        );
 
         $cookie = SetCookie::thatDeletesCookie('name');
         $expected = sprintf('name=deleted; expires=%s', gmdate(self::$HTTP_DATE_FORMAT, 1));
-        $this->assertEquals($expected, $cookie->toHeaderValue());
+        self::assertEquals($expected, $cookie->toHeaderValue());
 
         $now = new DateTimeImmutable();
         $cookie = SetCookie::thatExpires('name', 'value', $now);
-        $timestamp = (int) $now->format('U');
+        $timestamp = (int)$now->format('U');
         $expected = sprintf('name=value; expires=%s', gmdate(self::$HTTP_DATE_FORMAT, $timestamp));
-        $this->assertEquals($expected, $cookie->toHeaderValue());
+        self::assertEquals($expected, $cookie->toHeaderValue());
 
         $cookie = SetCookie::thatStaysForever('name', 'value', '/path/', 'domain.tld');
         $expiresInFiveYear = time() + 5 * 365 * 3600 * 24;
-        $expected = sprintf('name=value; expires=%s; path=/path/; domain=domain.tld', gmdate(self::$HTTP_DATE_FORMAT, $expiresInFiveYear));
-        $this->assertEquals($expected, $cookie->toHeaderValue());
+        $expected = sprintf(
+            'name=value; expires=%s; path=/path/; domain=domain.tld',
+            gmdate(self::$HTTP_DATE_FORMAT, $expiresInFiveYear)
+        );
+        self::assertEquals($expected, $cookie->toHeaderValue());
+    }
+
+    public function test_throws_exception_when_invalid_name()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        SetCookie::thatStaysForever('', '');
+    }
+
+    public function test_throws_exception_when_invalid_same_site()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        SetCookie::thatStaysForever(
+            'valid_name',
+            'valid_value',
+            '',
+            '',
+            false,
+            false,
+            'invalid_value'
+        );
     }
 }
