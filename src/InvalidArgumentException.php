f5620c58f4000e1731aee50edf477e05da089c36
Hans Ott <hansott@hotmail.be>
2020-07-19 11:52:29 +0200

    Revert "🗑 Add deprecation warning"
    This reverts commit 10151d7522980429e960b883e09b95a1c393172e.

--- src/InvalidArgumentException.php
+++ src/InvalidArgumentException.php
@@ -5,9 +5,6 @@ namespace HansOtt\PSR7Cookies;
 
 use Exception;
 
-/**
- * @deprecated Exception is not used and will be removed in future versions
- */
 final class InvalidArgumentException extends Exception
 {
 }
